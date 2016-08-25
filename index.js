b932b6c347318f2892f3faeadeb191683776f426
Bryan Wood <itanex@gmail.com>
2016-08-25 00:37:49 -0700

    add post and get for tweets

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 
 var app = express();
+app.listen(8000);
+
+var tweets = [];
 
 app.get('/', function(req, res){
     res.send('Welcome to Node Twitter');
 });
 
-app.listen(8000);
+app.post('/send', function(req, res){
+    if(req.body && red.body.tweet) {
+        tweets.push(req.body.tweet);
+        res.send({status: "ok", message: "Tweet Received"});
+    } else {
+        // no tweet?
+        res.send({status: "nok", message: "No tweet received"});
+    }
+});
+
+app.get('/tweets', function(req, res){
+    res.send(tweets);
+});
